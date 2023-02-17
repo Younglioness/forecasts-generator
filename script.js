327ffdd7224d0d1089caf13d71d8271437d838e7
Younglioness <123167319+Younglioness@users.noreply.github.com>
2023-02-17 21:12:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,39 @@
 /* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
 
 /* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
+
+const button = document.querySelector('.forecast-btn');
+const forecast = document.querySelector('h1');
+const percentage = document.querySelector('p');
+const titleContainer = document.querySelector('.container')
+
+button.addEventListener('click', function() {
+    let num = getRandom(1, 4);
+    switch (num) {
+        case 1:
+            forecast.textContent = "Грядут важные события";
+            break;
+        case 2:
+            forecast.textContent = "Ждите, не плача, придет к Вам удача";
+            break;
+        case 3:
+            forecast.textContent = "Вас ждет приятный сюрприз";
+            break;
+    }
+    let percentage = getRandom(1, 101) + '%';
+
+    function createNewForecast(title, text) {
+        const template = document.querySelector("#forecast-item");
+        const newForeCast = template.content.cloneNode(true);
+        newForeCast.querySelector("h3").textContent = title;
+        newForeCast.querySelector("p").textContent = text;
+        titleContainer.append(newForeCast);
+    }
+
+    createNewForecast(forecast.textContent, percentage);
+})
+
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
